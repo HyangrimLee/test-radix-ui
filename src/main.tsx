1bd8258738ca4e3355e9ff24dee6bb99304d2991
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2026-07-07 15:25:28 +0900

    기본구성

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-import './index.css'
+import '@/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import HomeLayout from "../layouts/HomeLayout";
-import ThemeLayout from "../layouts/ThemeLayout";
-import Home from "../pages/Home";
-import ThemeHome from "../pages/Theme/Home";
-import Card from "../pages/Theme/Card";
-// import ThemePage from "../pages/ThemePage";
-// import HeadlessPage from "../pages/HeadlessPage";
+import HomeLayout from "@/layouts/HomeLayout";
+import ThemeLayout from "@/layouts/ThemeLayout";
+import HeadlessLayout from "@/layouts/HeadLessLayout";
+import Home from "@/pages/Home";
+
+// Theme
+import ThemeHome from "@/pages/Theme/Home";
+import Card from "@/pages/Theme/Card";
+
+// Headless
+import HeadlessHome from "@/pages/Headless/Home";
+import PopOver from "@/pages/Headless/PopOver";
 
 export const router = createBrowserRouter([
   {
     element: <HomeLayout />,
     children: [
       { path: "/", element: <Home /> },
-    //   { path: "/theme", element: <ThemePage /> },
-    //   { path: "/headless", element: <HeadlessPage /> },
     ],
   },
   {
@@ -22,9 +25,13 @@ export const router = createBrowserRouter([
     children: [
       { path: "/theme", element: <ThemeHome /> },
       { path: "/theme/card", element: <Card /> },
-    //   { path: "/headless", element: <HeadlessPage /> },
-    ]
-
-  }
-  
+    ],
+  },
+  {
+    element: <HeadlessLayout />,
+    children: [
+      { path: "/headless", element: <HeadlessHome /> },
+      { path: "/headless/popover", element: <PopOver /> },
+    ],
+  },
 ]);
