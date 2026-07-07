3657109608fe1609e354929e9d22737a18644c13
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2026-07-07 16:06:27 +0900

    [HeadLess] 팝오버: react-router-dom 사용

--- src/pages/Headless/Home.tsx
+++ src/pages/Headless/Home.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
         <div>Home</div>
         <div>
-            <a href="/headless/popover">PopOver</a>
+            <Link to="/headless/popover">PopOver</Link>
         </div>
         
 
