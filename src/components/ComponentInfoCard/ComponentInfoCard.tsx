e57099aaa2a3612ccacc5cc7a2b21ffad188f06d
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2026-07-10 15:03:52 +0900

    [컴포넌트] 컴포넌트 소개 카드: 콘텐츠 내용에 따라 높이 조절

--- src/components/ComponentInfoCard/ComponentInfoCard.tsx
+++ src/components/ComponentInfoCard/ComponentInfoCard.tsx
@@ -11,23 +11,17 @@ export default function ComponentInfoCard(props: ComponentInfoCardProps) {
     return (
         <Link href={props.href} underline="none">
             <Card size="1">
-                <Flex gap="3" align="start">
-                <Avatar
-                    size="3"
-                    radius="full"
-                    color="lime"
-                    fallback={props.title[0]}
-                />
-
-                <Box>
-                    <Text as="div" size="2" weight="bold">
-                    {props.title}
-                    </Text>
-
-                    <Text as="div" size="2" color="gray">
-                    {props.description}
-                    </Text>
-                </Box>
+                <Flex gap="3" align="start" minHeight="76px" maxHeight="64px" pb="2" overflowY="auto">
+                    <Avatar
+                        size="3"
+                        radius="full"
+                        color="lime"
+                        fallback={props.title[0]}
+                    />
+                    <Box>
+                        <Text as="div" size="2" weight="bold">{props.title}</Text>
+                        <Text as="div" size="2" color="gray">{props.description}</Text>
+                    </Box>
                 </Flex>
             </Card>
         </Link>
